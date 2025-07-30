009b51aedbe8be44b7134974191e94a8035a51ec
IDEA <jjggsfx@gmail.com>
2025-07-31 09:09:32 +1000

    make a V2

---
+++ v02/config.py
@@ -0,0 +1,3 @@
+hidden_layer =  100
+alfa = 0.001
+itr = 2
--- v02/main.py
+++ v02/main.py
@@ -1,6 +1,18 @@
 import numpy as np 
 import os
 from addon import get_sentence_by_file , make_word_list
+from config import hidden_layer , alfa , itr
 
-sentences = get_sentence_by_file("realistic_english_sentences_5000.txt")
+sentences = get_sentence_by_file("realistic_english_sentences_5000.txt") #about len = ( min = 4  max =8 ) 
+words = make_word_list(sentences=sentences)
 
+one_matrix =  np.eye(hidden_layer)
+
+def learning():
+    min = 6 
+    max = 0
+    for i in itr: 
+        for snt in sentences : #example of snt = ['they', 'attend', 'meetings', 'every', 'monday'] 
+            
+
+learning()
